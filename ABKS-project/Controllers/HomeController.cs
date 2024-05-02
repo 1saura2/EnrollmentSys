2f8da1ce167e776ec636fc69ecd9b1ca1ab08b6d
Atul Baral <atul.baral8421@gmail.com>
2024-05-02 07:26:15 +0545

    register for unverified users

--- ABKS-project/Controllers/HomeController.cs
+++ ABKS-project/Controllers/HomeController.cs
@@ -1,43 +1,74 @@
 ﻿using ABKS_project.Models;
+using ABKS_project.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using System;
+using System.IO;
 
 namespace ABKS_project.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly abksContext context;
+        private readonly IWebHostEnvironment env;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(abksContext context, IWebHostEnvironment env)
         {
-            _logger = logger;
+            this.context = context;
+            this.env = env;
         }
 
         public IActionResult Index()
         {
             return View();
-        }     
+        }
+
         public IActionResult Login()
         {
             return View();
         }
+
         public IActionResult Register()
-        {
-            return View();
-        } 
-        public IActionResult Product()
         {
             return View();
         }
 
-        
-
-
-
-            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public IActionResult Register(UserViewModel usr)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (ModelState.IsValid)
+            {
+                string fileName = "";
+                if (usr.Photo != null)
+                {
+                    string folder = Path.Combine(env.WebRootPath, "Images");
+                    fileName = Guid.NewGuid().ToString() + "_" + usr.Photo.FileName;
+                    string filePath = Path.Combine(folder, fileName);
+                    usr.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+
+                    User user = new User()
+                    {
+                        FullName = usr.FullName,
+                        Email = usr.Email,
+                        Age = usr.Age,
+                        ContactNumber = usr.ContactNumber,
+                        Education = usr.Education,
+                        CitizenshipPhoto = fileName,
+                        IsVerified = false
+                    };
+                    context.Users.Add(user);
+                    context.SaveChanges();
+                    TempData["Register_Success"] = "Registered successfully.";
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(usr);
+        }
+
+
+        public IActionResult Product()
+        {
+            return View();
         }
     }
 }
